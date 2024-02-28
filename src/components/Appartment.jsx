91af525f512f0f816fe47a516e670e85c4323fb5
rahma717 <rahmabouabid2@gmail.com>
2024-02-28 10:23:16 +0100

    ajout dune div overlay dans appartment jsx

--- src/components/Appartment.jsx
+++ src/components/Appartment.jsx
@@ -7,6 +7,7 @@ function Appartment({ logement }) {
     <div className="appartment">
          {/* Utilisation de Link pour créer un lien vers la page détaillée du logement */}
     <Link to={`/logement/${logement.id}`}>
+        <div className="overlay"></div>
         {/* Affiche l'image du logement avec la source provenant de logement.cover et une alternative basée sur le titre du logement */}
      <img src={logement.cover} alt={logement.title} />
       {/* Affiche le titre du logement */}
